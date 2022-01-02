2ad12f637ba5717f697934a213439ae45e317dad
Phebian Chukwurah <31128822+Ph3bian@users.noreply.github.com>
2022-01-02 22:18:05 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.scss";
-import store from "./store";
 import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
